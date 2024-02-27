3860a85ffb5cd4773a86a02bc1ad7fa503460fd3
Muhamad Nabil Ihsani <nabil@Muhamads-MacBook-Pro.local>
2024-02-27 17:23:30 +0700

    initial commit

---
+++ 01-Fundamentals-Part-1/starter/assignment.js
@@ -0,0 +1,17 @@
+// Values and Variables Assignment
+let country = "Indonesia";
+let continent = "Asia";
+let population = 273;
+
+console.log(country);
+console.log(continent);
+console.log(population);
+
+// Data Types Assignment
+let isIsland = true;
+let language;
+
+console.log(typeof isIsland);
+console.log(typeof population);
+console.log(typeof country);
+console.log(typeof language);
